94a2d87c8ffd2784b389a163a40db536b42ae5b5
Karikiomy <118628350+Karikiomy@users.noreply.github.com>
2022-11-20 00:15:09 +0100

    Initial commit

---
+++ AddonName.lua
@@ -0,0 +1,13 @@
+--[[
+    https://us.forums.blizzard.com/en/wow/t/wow-classic-ui-api-change-for-unithealth/446596
+    https://www.townlong-yak.com/framexml/37176/GlobalStrings.lua/RU
+    https://en.wikipedia.org/wiki/International_System_of_Units#Prefixes
+    https://stackoverflow.com/questions/9461621/format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900
+    https://github.com/tomrus88/BlizzardInterfaceCode/blob/master/Interface/FrameXML/GameTooltip.lua#L94-L157
+    https://github.com/tomrus88/BlizzardInterfaceCode/blob/master/Interface/SharedXML/SharedColorConstants.lua#L73-L82
+    https://github.com/tomrus88/BlizzardInterfaceCode/blob/master/Interface/FrameXML/UIParent.lua#L5351-L5362
+]]
+
+local _, addonTable = ...
+local _c = addonTable.config
+local db = {} -- local database
---
+++ Config.lua
@@ -0,0 +1,12 @@
+--[[
+    https://wowpedia.fandom.com/wiki/Using_the_AddOn_namespace
+    https://wowpedia.fandom.com/wiki/WOW_PROJECT_ID
+    https://github.com/Stanzilla/WoWUIBugs/issues/68#issuecomment-830351390
+    https://github.com/tomrus88/BlizzardInterfaceCode/blob/master/Interface/SharedXML/SharedColorConstants.lua
+]]
+
+local _, addonTable = ...
+
+addonTable.config = {
+    property = true
+}
